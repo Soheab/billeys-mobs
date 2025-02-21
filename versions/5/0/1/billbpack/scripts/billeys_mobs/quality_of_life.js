d9e6b4b8c380278e86c30342251b2ee8f3fd47a0
Billey's Mobs Addon Unpacker <action@github.com>
2025-02-21 22:38:05 +0000

    Unpacked addon version 5.0.1

---
+++ versions/5/0/1/billbpack/scripts/billeys_mobs/quality_of_life.js
@@ -0,0 +1,195 @@
+import { world, system, Player, Entity, BlockVolume } from "@minecraft/server";
+import { ModalFormData } from "@minecraft/server-ui";
+import { DIMENSIONS } from "./utility";
+
+world.afterEvents.entityDie.subscribe(
+    ({ deadEntity }) => {
+        if (!deadEntity.isValid())
+            return;
+        const chunkLoader = deadEntity.dimension.spawnEntity(
+            "billey:chunk_loader",
+            deadEntity.location
+        );
+        const subscription = world.afterEvents.playerSpawn.subscribe(({ player }) => {
+            if (player.id != deadEntity.id)
+                return;
+            world.afterEvents.playerSpawn.unsubscribe(subscription);
+            tpAllFollowingPetsUsingStructure(player, chunkLoader);
+        });
+    },
+    {
+        entityTypes: ["minecraft:player"]
+    }
+);
+
+/** @param {Player} player  */
+export async function showSettingForm(player) {
+    const form = new ModalFormData()
+        .title({ translate: "ui.billeys_mobs.info.category.settings" })
+        .toggle(
+            { translate: "ui.billeys_mobs.settings.can_hit_own_pet" },
+            player.hasTag("billey:can_hit_own_pet")
+        )
+        .toggle(
+            { translate: "ui.billeys_mobs.settings.can_hit_other_pet" },
+            !player.hasTag("billey:cant_hit_other_pet")
+        );
+    const { canceled, formValues } = await form.show(player);
+    if (canceled) return;
+    if (formValues[0])
+        player.addTag("billey:can_hit_own_pet");
+    else
+        player.removeTag("billey:can_hit_own_pet");
+    if (formValues[1])
+        player.removeTag("billey:cant_hit_other_pet");
+    else
+        player.addTag("billey:cant_hit_other_pet");
+}
+
+let nextXPushDirection = 1;
+let nextZPushDirection = 1;
+/** 
+ * @param {Player} player The owner of the pets
+ * @param {boolean} alwaysUnjumble Push the pet around even if it's only one
+*/
+export function tpAllFollowingPets(player, alwaysUnjumble) {
+    for (const dimension of DIMENSIONS) {
+        const followingPets = dimension
+            /**
+             * the tamed tag is added to all tamed billey mobs.
+             * it was used back in the day when scripts didn't yet exist
+             * to allow commands to detect if a mob is tamed or not,
+             * eg. angel cats giving regeneration.
+             * Here it wasn't needed but probably
+             * slightly optimizes this function.
+             */
+            .getEntities({
+                tags: ["tamed"]
+            })
+            .filter(entity =>
+                entity.getProperty("billey:is_sitting") === false
+                && entity.getComponent("tameable")?.tamedToPlayerId == player.id
+                && isFollowingOwner(entity)
+            );
+        for (const pet of followingPets) {
+            pet.teleport(player.location, { dimension: player.dimension });
+            if (!alwaysUnjumble && followingPets.length == 1) continue;
+            const x = Math.max(0.1, Math.random() / 2) * nextXPushDirection;
+            const z = Math.max(0.1, Math.random() / 2) * nextZPushDirection;
+            if (Math.random() < 0.5)
+                nextXPushDirection *= -1;
+            else
+                nextZPushDirection *= -1;
+            pet.applyImpulse({ x, y: 0, z });
+        }
+        //removeWaystoneLoader(player);
+    }
+}
+
+/** 
+ * @param {Player} player The owner of the pets
+ * @param {Entity} chunkLoader 
+ * Teleports all following pets by saving them as structures, deleting them, and then 
+ * placing the structure. This fixes the invisible pet after tp vanilla bug
+*/
+export function tpAllFollowingPetsUsingStructure(player, chunkLoader, doNotRepeat) {
+    for (const dimension of DIMENSIONS) {
+        const followingPets = dimension
+            /**
+             * the tamed tag is added to all tamed billey mobs.
+             * it was used back in the day when scripts didn't yet exist
+             * to allow commands to detect if a mob is tamed or not,
+             * eg. angel cats giving regeneration.
+             * Here it wasn't needed but probably
+             * slightly optimizes this function.
+             */
+            .getEntities({
+                tags: ["tamed"]
+            })
+            .filter(entity =>
+                entity.getProperty("billey:is_sitting") === false
+                && entity.getComponent("tameable")?.tamedToPlayerId == player.id
+                && isFollowingOwner(entity)
+            );
+        for (const pet of followingPets) {
+            pet.teleport(player.location, { dimension: player.dimension });
+            pet.setDynamicProperty("unjumble_on_load", true);
+        }
+        //removeWaystoneLoader(player);
+    }
+    const { dimension } = player;
+    const structureId = `billey:${player.id}_tped_pets`;
+    world.structureManager.delete(structureId);
+    world.structureManager.createFromWorld(
+        structureId,
+        dimension, player.location, player.location
+    );
+    dimension.getEntitiesAtBlockLocation(player.location)
+        .forEach(e => { if (!(e instanceof Player)) e.remove(); });
+    world.structureManager.place(structureId, dimension, player.location, {
+        includeBlocks: false
+    });
+    world.structureManager.delete(structureId);
+    chunkLoader?.remove();
+    system.run(() => {
+        if (!doNotRepeat)
+            tpAllFollowingPetsUsingStructure(player, undefined, true);
+    });
+}
+
+world.afterEvents.entityLoad.subscribe(({ entity }) => {
+    if (entity.getDynamicProperty("unjumble_on_load")) {
+        const x = Math.max(0.1, Math.random() / 2) * nextXPushDirection;
+        const z = Math.max(0.1, Math.random() / 2) * nextZPushDirection;
+        if (Math.random() < 0.5)
+            nextXPushDirection *= -1;
+        else
+            nextZPushDirection *= -1;
+        entity.applyImpulse({ x, y: 0, z });
+        entity.setDynamicProperty("unjumble_on_load", undefined);
+    }
+});
+
+/** 
+ * @param {Entity} pet
+ */
+function isFollowingOwner(pet) {
+    if (!pet.isValid()) {
+        world.sendMessage(`§cError: The '${pet.typeId}' was not actually loaded. §ePlease let Bill know.`);
+    }
+    const followOwnerState = pet.getProperty("billey:follow_owner_state");
+    if (followOwnerState == "unknown") {
+        world.sendMessage(`§cError: Property 'billey:follow_owner_state' was 'unknown' on '${pet.nameTag}' the '${pet.typeId}' even though it was tamed. §ePlease let Bill know.`);
+    }
+    return followOwnerState == "following";
+}
+
+world.afterEvents.playerDimensionChange.subscribe(({ player }) => {
+    tpAllFollowingPetsUsingStructure(player);
+});
+
+/** @param {Player} player 
+export function removeWaystoneLoader(player) {
+    const waystoneLoader = player.__waystoneLoader;
+    /** @type {Entity|undefined}
+    if (waystoneLoader?.isValid())
+        waystoneLoader.remove();
+    player.__waystoneLoader = undefined;
+} */
+
+world.afterEvents.entityLoad.subscribe(({ entity }) => {
+    if (entity.typeId == "billey:chunk_loader")
+        entity.remove();
+});
+
+/*world.afterEvents.playerInteractWithBlock.subscribe(({ block, player }) => {
+    if (!block.isValid() || !block.typeId.includes("waystone"))
+        return;
+    removeWaystoneLoader(player);
+    player.__waystoneLoader = player.dimension.spawnEntity(
+        "billey:chunk_loader",
+        block.location
+    );
+});*/
+
+//world.sendMessage(world.structureManager.getWorldStructureIds().join("\n"))
