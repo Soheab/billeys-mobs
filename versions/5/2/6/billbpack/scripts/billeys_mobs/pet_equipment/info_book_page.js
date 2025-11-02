58880ac2c43b52cb2fce82601ee41c42a514fe7e
Billey's Mobs Addon Unpacker <action@github.com>
2025-11-02 21:55:13 +0000

    Unpacked addon version 5.2.6

---
+++ versions/5/2/6/billbpack/scripts/billeys_mobs/pet_equipment/info_book_page.js
@@ -0,0 +1,41 @@
+import { Player } from "@minecraft/server";
+import { getAllPetEquipmentIds } from "./registry";
+import { ActionFormData } from "@minecraft/server-ui";
+import { showInfoBookForm } from "../info_book";
+
+
+
+/**
+ * @param {Player} player 
+ */
+export async function showPetEquipmentInfoForm(player) {
+    const form = new ActionFormData();
+    form.title({ translate: "ui.billeys_mobs.pet_equipment" });
+    form.body({ translate: "ui.billeys_mobs.pet_equipment.body", with: ["\n"] });
+    const petEquipmentIds = getAllPetEquipmentIds().filter(id => id.startsWith("billey:"));
+    for (const id of petEquipmentIds) {
+            //id.slice(7) removes the billey:
+            form.button(`item.${id}`, "textures/billeyitems/" + id.slice(7));
+    }
+    form.button({ translate: "gui.back" });
+    const { selection, canceled } = await form.show(player);
+    if (canceled)
+        return;
+    else if (selection == petEquipmentIds.length) {
+        showInfoBookForm(player);
+        return;
+    }
+    else {
+        const form = new ActionFormData();
+        /** @type {string} */
+        const id = petEquipmentIds[selection];
+        form.title({ translate: `item.${id}` });
+        form.body({ translate: "ui.billeys_mobs.item_info." + id.slice(7), with: ["\n"] });
+        form.button({ translate: "gui.back" });
+        if (
+            (await form.show(player)).selection === 0
+        ) {
+            showPetEquipmentInfoForm(player);
+        }
+    }
+}
